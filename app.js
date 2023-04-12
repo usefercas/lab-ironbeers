eb4221508265a2e77f115bbd7cf0b4ddaeb22e27
usefercas <124436934+usefercas@users.noreply.github.com>
2023-04-12 13:30:10 +0200

    ejercicio resuelto

--- app.js
+++ app.js
@@ -14,12 +14,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
+
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/randomBeer', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      let numeroEntreCeroYVeinteCuatro = Math.round(Math.random() * 4);
+      let beer = beersFromApi[numeroEntreCeroYVeinteCuatro];
+      console.log("esta es la cerveza :" + beer.name);
+      res.render('randomBeer', { beer });
+    })
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
